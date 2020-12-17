d5ac6df95a0ca934769b713c5119e01b41dd518d
mohamed <mohamed.hatem100100@gmail.com>
2020-12-17 17:18:35 +0200

    seeting push

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -8,7 +8,7 @@
             </div>
             <div class="content-body">
                 <div id="crypto-stats-3" class="row">
-                    <div class="col-xl-4 col-12">
+                    <div class="col-xl-3 col-12">
                         <div class="card crypto-card-3 pull-up">
                             <div class="card-content">
                                 <div class="card-body pb-0">
@@ -17,12 +17,10 @@
                                             <h1><i class="cc BTC warning font-large-2" title="BTC"></i></h1>
                                         </div>
                                         <div class="col-5 pl-2">
-                                            <h4>BTC</h4>
-                                            <h6 class="text-muted">Bitcoin</h6>
+                                            <h4>اجمالي المبيعات</h4>
                                         </div>
                                         <div class="col-5 text-right">
                                             <h4>$9,980</h4>
-                                            <h6 class="success darken-4">31% <i class="la la-arrow-up"></i></h6>
                                         </div>
                                     </div>
                                 </div>
@@ -34,7 +32,33 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-xl-4 col-12">
+
+                    <div class="col-xl-3 col-12">
+                        <div class="card crypto-card-3 pull-up">
+                            <div class="card-content">
+                                <div class="card-body pb-0">
+                                    <div class="row">
+                                        <div class="col-2">
+                                            <h1><i class="cc BTC warning font-large-2" title="BTC"></i></h1>
+                                        </div>
+                                        <div class="col-5 pl-2">
+                                            <h4>اجالي الطلبات</h4>
+                                        </div>
+                                        <div class="col-5 text-right">
+                                            <h4>$9,980</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-12">
+                                        <canvas id="btc-chartjs" class="height-75"></canvas>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-xl-3 col-12">
                         <div class="card crypto-card-3 pull-up">
                             <div class="card-content">
                                 <div class="card-body pb-0">
@@ -43,12 +67,10 @@
                                             <h1><i class="cc ETH blue-grey lighten-1 font-large-2" title="ETH"></i></h1>
                                         </div>
                                         <div class="col-5 pl-2">
-                                            <h4>ETH</h4>
-                                            <h6 class="text-muted">Ethereum</h6>
+                                            <h4>عدد المنتجات </h4>
                                         </div>
                                         <div class="col-5 text-right">
-                                            <h4>$944</h4>
-                                            <h6 class="success darken-4">12% <i class="la la-arrow-up"></i></h6>
+                                            <h4>30</h4>
                                         </div>
                                     </div>
                                 </div>
@@ -60,7 +82,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-xl-4 col-12">
+                    <div class="col-xl-3 col-12">
                         <div class="card crypto-card-3 pull-up">
                             <div class="card-content">
                                 <div class="card-body pb-0">
@@ -69,12 +91,10 @@
                                             <h1><i class="cc XRP info font-large-2" title="XRP"></i></h1>
                                         </div>
                                         <div class="col-5 pl-2">
-                                            <h4>XRP</h4>
-                                            <h6 class="text-muted">Balance</h6>
+                                            <h4>عدد العملاء</h4>
                                         </div>
                                         <div class="col-5 text-right">
-                                            <h4>$1.2</h4>
-                                            <h6 class="danger">20% <i class="la la-arrow-down"></i></h6>
+                                            <h4>100</h4>
                                         </div>
                                     </div>
                                 </div>
@@ -94,7 +114,7 @@
                     <div class="col-12 col-xl-6">
                         <div class="card">
                             <div class="card-header">
-                                <h4 class="card-title">Sell Order</h4>
+                                <h4 class="card-title">احدث الطلبات</h4>
                                 <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
                                 <div class="heading-elements">
                                     <p class="text-muted">Total BTC available: 6542.56585</p>
@@ -105,42 +125,22 @@
                                     <table class="table table-de mb-0">
                                         <thead>
                                         <tr>
-                                            <th>Price per BTC</th>
-                                            <th>BTC Ammount</th>
-                                            <th>Total($)</th>
+                                            <th>رقم الطلب  </th>
+                                            <th>العميل</th>
+                                            <th>السعر</th>
+                                            <th>حالو الطلب</th>
+                                            <th>الاجمالي</th>
                                         </tr>
                                         </thead>
                                         <tbody>
                                         <tr class="bg-success bg-lighten-5">
-                                            <td>10583.4</td>
-                                            <td><i class="cc BTC-alt"></i> 0.45000000</td>
+                                            <td>10583</td>
+                                            <td><i class="cc BTC-alt"></i> منيسي</td>
                                             <td>$ 4762.53</td>
+                                            <td>Compeleted</td>
+                                            <td>$ 2762.53</td>
                                         </tr>
-                                        <tr>
-                                            <td>10583.5</td>
-                                            <td><i class="cc BTC-alt"></i> 0.04000000</td>
-                                            <td>$ 423.34</td>
-                                        </tr>
-                                        <tr>
-                                            <td>10583.7</td>
-                                            <td><i class="cc BTC-alt"></i> 0.25100000</td>
-                                            <td>$ 2656.51</td>
-                                        </tr>
-                                        <tr>
-                                            <td>10583.8</td>
-                                            <td><i class="cc BTC-alt"></i> 0.35000000</td>
-                                            <td>$ 3704.33</td>
-                                        </tr>
-                                        <tr>
-                                            <td>10595.7</td>
-                                            <td><i class="cc BTC-alt"></i> 0.30000000</td>
-                                            <td>$ 3178.71</td>
-                                        </tr>
-                                        <tr class="bg-danger bg-lighten-5">
-                                            <td>10599.5</td>
-                                            <td><i class="cc BTC-alt"></i> 0.02000000</td>
-                                            <td>$ 211.99</td>
-                                        </tr>
+                                   
                                         </tbody>
                                     </table>
                                 </div>
@@ -150,7 +150,7 @@
                     <div class="col-12 col-xl-6">
                         <div class="card">
                             <div class="card-header">
-                                <h4 class="card-title">Buy Order</h4>
+                                <h4 class="card-title">اخر التقيمات</h4>
                                 <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
                                 <div class="heading-elements">
                                     <p class="text-muted">Total USD available: 9065930.43</p>
@@ -161,42 +161,18 @@
                                     <table class="table table-de mb-0">
                                         <thead>
                                         <tr>
-                                            <th>Price per BTC</th>
-                                            <th>BTC Ammount</th>
-                                            <th>Total($)</th>
+                                            <th>العميل</th>
+                                            <th>المنتج</th>
+                                            <th>التقيم</th>
                                         </tr>
                                         </thead>
                                         <tbody>
                                         <tr class="bg-danger bg-lighten-5">
-                                            <td>10599.5</td>
-                                            <td><i class="cc BTC-alt"></i> 0.02000000</td>
-                                            <td>$ 211.99</td>
+                                            <td>محمد منيسي</td>
+                                            <td><i class="cc BTC-alt"></i> ساعة يد</td>
+                                            <td>3</td>
                                         </tr>
-                                        <tr>
-                                            <td>10583.5</td>
-                                            <td><i class="cc BTC-alt"></i> 0.04000000</td>
-                                            <td>$ 423.34</td>
-                                        </tr>
-                                        <tr>
-                                            <td>10583.8</td>
-                                            <td><i class="cc BTC-alt"></i> 0.35000000</td>
-                                            <td>$ 3704.33</td>
-                                        </tr>
-                                        <tr>
-                                            <td>10595.7</td>
-                                            <td><i class="cc BTC-alt"></i> 0.30000000</td>
-                                            <td>$ 3178.71</td>
-                                        </tr>
-                                        <tr class="bg-danger bg-lighten-5">
-                                            <td>10583.7</td>
-                                            <td><i class="cc BTC-alt"></i> 0.25100000</td>
-                                            <td>$ 2656.51</td>
-                                        </tr>
-                                        <tr>
-                                            <td>10595.8</td>
-                                            <td><i class="cc BTC-alt"></i> 0.29697926</td>
-                                            <td>$ 3146.74</td>
-                                        </tr>
+                 
                                         </tbody>
                                     </table>
                                 </div>
